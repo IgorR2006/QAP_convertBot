bf3cf1aabe9dbc0990526193175d307f9931f671
Igor Rum <rumgor@yandex.ru>
2023-11-27 14:50:21 +0300

    add class EexchangeConverter

--- app.py
+++ app.py
@@ -18,6 +18,35 @@ keys = {
 class ConvertionException(Exception):
     pass
 
+class EexchangeConverter:
+    @staticmethod
+    def convert(quote: str, base: str, amount: str):
+               
+        if quote == base:
+            raise ConvertionException(f'Невозможно перевести одинаковые валлюты {base}')
+        
+        try:
+            quote_ticher = keys[quote]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту {quote}')
+        
+        try:
+            base_ticher = keys[base]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту {base}')
+        
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvertionException(f'Не удалось обработать количество {amount}')
+        
+                
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticher}&tsyms={base_ticher}')
+        total_base = json.loads(r.content)[keys[base]]
+        
+        return total_base
+        
+
 @bot.message_handler(commands=['start', 'help'])
 def help(message: telebot.types.Message):
     text = 'Чтобы начать работу введите команду боту в следующем формате: \n <имя валюты> <в какуювалюту перевести>\
@@ -35,30 +64,19 @@ def help(message: telebot.types.Message):
 
 @bot.message_handler(content_types=['text', ])
 def convert(message: telebot.types.Message):
+        
     low_text = message.text.lower()
     values = low_text.split(' ')
     
-    if len(values) > 3:
+    if len(values) != 3:
         raise ConvertionException('Слишком много параметров')
-           
+            
     quote, base, amount = values
+    total_base = EexchangeConverter.convert(quote, base, amount)
     
-    if quote == base:
-        raise ConvertionException(f'Невозможно перевести одинаковые валлюты {base}')        
-    
-    r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={keys[quote]}&tsyms={keys[base]}')
-    total_base = json.loads(r.content)[keys[base]]
     convert_amout = float(total_base) * float(amount)
     text = f'Цена {amount} {quote} в {base} - {round(convert_amout, 2)}'
     bot.send_message(message.chat.id, text)
-
-
-
-
-
-# @bot.message_handler()
-# def echo_test(message: telebot.types.Message):
-#     bot.send_message(message.chat.id, 'HELLOOO')
 
 
 bot.polling()
