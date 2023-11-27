e34efe4523d18629ca61aaba8deea9cec642ab24
Igor Rum <rumgor@yandex.ru>
2023-11-27 15:32:31 +0300

    finish

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def help(message: telebot.types.Message):
-    text = 'Чтобы начать работу введите команду боту в следующем формате: \n <имя валюты> <в какуювалюту перевести>\
+    text = 'Чтобы начать работу введите команду боту в следующем формате: \n <имя валюты> <в какую валюту перевести> \
     <количество переводимой валюты> \n Увидеть список всех доступных валют: /values'
     bot.reply_to(message, text)
 
@@ -23,19 +23,26 @@ def help(message: telebot.types.Message):
 
 @bot.message_handler(content_types=['text', ])
 def convert(message: telebot.types.Message):
+    try:
+        low_text = message.text.lower()
+        values = low_text.split(' ')
         
-    low_text = message.text.lower()
-    values = low_text.split(' ')
-    
-    if len(values) != 3:
-        raise ConvertionException('Слишком много параметров')
-            
-    quote, base, amount = values
-    total_base = EexchangeConverter.convert(quote, base, amount)
-    
-    convert_amout = float(total_base) * float(amount)
-    text = f'Цена {amount} {quote} в {base} - {round(convert_amout, 2)}'
-    bot.send_message(message.chat.id, text)
+        if len(values) != 3:
+            raise ConvertionException('Слишком много параметров')
+                
+        quote, base, amount = values
+        total_base = EexchangeConverter.convert(quote, base, amount)
+        convert_amout = float(total_base) * float(amount)
+        
+    except ConvertionException as e:
+        bot.reply_to(message, f'Ошибка пользователя. Смотри инструкцию /help \n{e}')
+        
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду\n{e}')
+        
+    else:
+        text = f'Цена {amount} {quote} в {base} - {round(convert_amout, 2)}'
+        bot.send_message(message.chat.id, text)
 
 
 bot.polling()
